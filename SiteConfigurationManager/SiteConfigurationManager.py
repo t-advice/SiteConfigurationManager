eaf2580894a449f687d545ccbc7881e7d54ff9f2
Tashwill <tashwilladvice02@gmail.com>
2026-05-21 19:01:58 +0200

    # 1 Setting up a default dicationary profile default_config = {     "project_name": "UWC Dev Phase 1",     "max_users" : 5,     "dark_mode": True,     "api_version" : 1.4

--- SiteConfigurationManager/SiteConfigurationManager.py
+++ SiteConfigurationManager/SiteConfigurationManager.py
@@ -1,1 +1,18 @@
+import json
+import os
 
+filename = "site_config.json"
+
+print("Tashwill's System Configuration Engine")
+print("-" *30)
+
+# 1 Setting up a default dicationary profile
+default_config = {
+    "project_name": "UWC Dev Phase 1",
+    "max_users" : 5,
+    "dark_mode": True,
+    "api_version" : 1.4
+
+    }
+
+#
