f2dc605f7963a22301a2a8f63a452962e6112ff7
Tashwill <tashwilladvice02@gmail.com>
2026-05-21 19:13:47 +0200

    Reading the data back to check if it works print("Reading file back into memory...") if os.path.exists(filename):     with open(filename,"r") as file:               #json.load converts the plain text string back into a python dictionary               loaded_config = json.laod(file)

--- SiteConfigurationManager/SiteConfigurationManager.py
+++ SiteConfigurationManager/SiteConfigurationManager.py
@@ -15,4 +15,22 @@ default_config = {
 
     }
 
-#
+#2. Saving the directory as a JSON file
+print("Saving system configuration..")
+with open(filename, "w") as file:
+
+    json.dump(default_config, file, indent=4)
+print(f"Success! Settings saved to '{filename}'. ")
+print("-" *30)
+
+
+#3. Reading the data back to check if it works
+print("Reading file back into memory...")
+if os.path.exists(filename):
+    with open(filename,"r") as file:
+              #json.load converts the plain text string back into a python dictionary
+              loaded_config = json.laod(file)
+    print("\n--- Current Configuration Profile ---")
+    print(f"Project Workspace: {loaded_config['project_name']}")
+    print(f"Max Connections : {loaded_config['max_users']}")
+    print(f"UI Settings Dark: {loaded_config['dark_mode']}")
